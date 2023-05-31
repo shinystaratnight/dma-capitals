99888916d129d64c5a2078442c548efe1a0962f7
Rising Star <star-atnight@outlook.com>
2023-05-31 09:39:33 +0800

    Add bootstrap 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +17,7 @@ function App() {
           Learn React
         </a>
       </header>
+      </div>
     </div>
   );
 }
